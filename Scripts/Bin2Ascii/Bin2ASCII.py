cf27bd8f73e184a436432178e202c8cb4f3072c2
Chris Moreno <chris.mrn92@gmail.com>
2018-09-29 14:16:39 -0500

    VSCode

--- Scripts/Bin2Ascii/Bin2ASCII.py
+++ Scripts/Bin2Ascii/Bin2ASCII.py
@@ -3,8 +3,8 @@ iterador=0
 Acumulador=[]
 Palabra=''
 while i < len(LongNumber)+1:
-  Acumulador.append('0'+LongNumber[iterador=0:iterador=0+7])
-  iterador+=7
+	Acumulador.append('0'+LongNumber[iterador=0:iterador=0+7])
+	iterador+=7
 for x in Acumulador:
         Palabra=Palabra+chr(int(str(int(x, 2))))
 print Palabra
